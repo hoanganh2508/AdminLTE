1747c878abb3322d8af5370ad4de0a69058a97e8
hoanganh2508 <hoanganhle30081995@gmail.com>
2023-08-08 00:28:59 +0700

    gem devise and login logout anymore

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get 'dashbroad/index'
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
